8de921a648f1d9a8a5657ab12f19d4921f79ba80
MichaelAngel01 <2502016628@qq.com>
2021-08-22 02:19:37 +0800

    "上拉加载更多和回到顶部"

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,129 +1,44 @@
 <template>
   <div id="home">
-    <nav-bar class="home-nav">
-      <div slot="center">购物街</div>
-    </nav-bar>
-    <home-swiper :banners="banners"></home-swiper>
-    <recommend-view :recommends="recommends"></recommend-view>
-    <feature-view></feature-view>
-    <tab-control class="tabControl" :titles="['流行','新款','精选']"></tab-control>
-    <ul>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-      <li>列表</li>
-    </ul>
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    
+    <scroll class="content" 
+            ref="scroll" 
+            :probe-type="3" 
+            @srcoll="contentScroll"
+            :pull-up-load="true"
+            @pullingUp="loadMore">
+      <home-swiper :banners="banners"/>
+      <recommend-view :recommends="recommends"/>
+      <feature-view/>
+      <tab-control class="tabControl" 
+      :titles="['流行','新款','精选']"
+      @tabClick="tabClick"/>
+      <goods-list :goods="showGoods"/>
+    </scroll>
+
+    <back-top @click.native="backClick" v-show="isShowBackTop"/>
+    
   </div>
 </template>
 
 <script>
 import NavBar from 'components/common/navbar/NavBar'
 import TabControl from 'components/content/tabControl/TabControl'
-
+import GoodsList from 'components/content/goods/GoodsList'
+import Scroll from 'components/common/scroll/Scroll'
+import BackTop from 'components/content/backTop/BackTop'
 
 import HomeSwiper from './childComps/HomeSwiper'
 import RecommendView from './childComps/RecommendView'
 import FeatureView from './childComps/FeatureView'
 
 
-import {getHomeMultidata} from 'network/home.js'
 
+import {
+  getHomeMultidata,
+  getHomeGoods
+  } from 'network/home.js'
 
   export default{
     name:"Home",
@@ -133,6 +48,9 @@ import {getHomeMultidata} from 'network/home.js'
       RecommendView,
       FeatureView,
       TabControl,
+      GoodsList,
+      Scroll,
+      BackTop,
     },
     data() {
       return {
@@ -141,18 +59,84 @@ import {getHomeMultidata} from 'network/home.js'
         recommends:[],
         goods:{
           'pop':{page:0,list:[],},
-          'news':{page:0,list:[],},
+          'new':{page:0,list:[],},
           'sell':{page:0,list:[],},
-        }
+        },
+        currentType:'pop',
+        isShowBackTop:false,
+      }
+    },
+    computed:{
+      showGoods(){
+        return this.goods[this.currentType].list
       }
     },
     created() {
-      //1.请求多个数据
-      getHomeMultidata().then(res =>{
-        //this.result = res;
-        this.banners = res.data.banner.list;
-        this.recommends = res.data.recommend.list;
-      })
+      this.getHomeMultidata()
+      this.getHomeGoods('pop')
+      this.getHomeGoods('new')
+      this.getHomeGoods('sell')
+    },
+    mounted() {
+      
+    },
+    methods: {
+      //上拉加载更多
+      loadMore(){
+        console.log('上拉加载更多');
+        this.getHomeGoods(this.currentType);
+        this.$refs.scroll.scroll.refresh();
+      },
+      //backTop到一定位置才会显示
+      contentScroll(position){
+        // console.log(position);
+        this.isShowBackTop = -position.y > 1000
+      },
+      //回到顶部
+      backClick(){
+        // this.$refs.scroll.scroll.scrollTo(0,0,500);//拿到scroll组件的srcoll对象调用bscroll的scrollTo方法
+        this.$refs.scroll.scrollTo(0,0,500)
+      },
+      /**
+       * 时间监听相关方法
+       */
+      tabClick(index){
+        switch(index){
+          case 0:
+            this.currentType = 'pop'
+            break
+            case 1:
+              this.currentType = 'new'
+              break
+            case 2:
+              this.currentType = 'sell'
+              break
+        }
+      },
+
+      /**
+       * 网络请求相关方法
+       */
+      getHomeMultidata(){
+        //1.请求多个数据
+        getHomeMultidata().then(res =>{
+          //this.result = res;
+          this.banners = res.data.banner.list;
+          this.recommends = res.data.recommend.list;
+        })
+      },
+
+      getHomeGoods(type){
+        //2.请求商品数据
+        const page = this.goods[type].page + 1;
+        getHomeGoods(type,page).then(res =>{
+          this.goods[type].list.push(...res.data.list);
+          this.goods[type].page += 1;
+
+          this.$refs.scroll.finishPullUp()
+        })
+      },
+
     },
   }
 </script>
@@ -160,6 +144,8 @@ import {getHomeMultidata} from 'network/home.js'
 <style scoped>
 #home{
   padding-top: 44px;
+  height: 100vh;
+  position: relative;
 }
 .home-nav{
   background-color: var(--color-tint);
@@ -169,10 +155,24 @@ import {getHomeMultidata} from 'network/home.js'
   left: 0;
   right: 0;
   top: 0;
-  z-index: 9px;
+  z-index: 9;
 }
 .tabControl{
   position:sticky;
   top: 44px;
+  z-index: 9;
 }
+.content{
+  overflow: hidden;
+  position:absolute;
+  top: 44px;
+  bottom: 49px;
+  left: 0;
+  right: 0;
+}
+/* .content{
+  height: cal(100%-93px);
+  overflow: hidden;
+  margin-top: 44px;
+} */
 </style>
--- src/views/home/childComps/FeatureView.vue
+++ src/views/home/childComps/FeatureView.vue
@@ -3,7 +3,6 @@
     <a href="https://act.mogujie.com/zzlx67">
       <img src="~assets/img/home/recommend_bg.jpg" alt="">
     </a>
-
   </div>
 </template>
 
