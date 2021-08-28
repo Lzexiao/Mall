b8e8e50ec1ea5bcf9a8446e9cdc35004b7ef2fec
MichaelAngel01 <2502016628@qq.com>
2021-08-28 23:03:40 +0800

    “

--- src/views/profile/Profile.vue
+++ src/views/profile/Profile.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="profile">
-    <nav-bar class="nav-bar"><div slot="center">小码哥商城</div></nav-bar>
+    <nav-bar class="nav-bar"><div slot="center">我的蘑菇街</div></nav-bar>
     <!--1.单独封装一个组件: 利用slot知识点-->
     <user-info></user-info>
 
